d2e426b67cb027176ca423df0e20fc4310ea8c02
fanwei <fwshun8023@gmail.com>
2016-07-31 10:34:31 +0800

    mysql utf8 to utf8mb4

--- db/migrate/20160607135454_create_article_table.rb
+++ db/migrate/20160607135454_create_article_table.rb
@@ -4,7 +4,7 @@ class CreateArticleTable < ActiveRecord::Migration
       t.string :title
       t.text :content
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 20160610092152) do
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.text     "content",    limit: 65535
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.integer  "user_id",    limit: 4
   end
 
